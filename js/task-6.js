5a583e332d469e105abc44391ff37be77a4f03e8
unknown <senchuk1205@gmail.com>
2019-11-07 17:50:10 +0200

    add_module_2_hw_vv

--- js/task-6.js
+++ js/task-6.js
@@ -3,19 +3,22 @@
 let input;
 const numbers = [];
 let total = 0;
+
 do {
   input = prompt("Введите число:");
   if (isNaN(input) || input === "") {
     alert("Было введено не число, попробуйте еще раз");
-  } else {
+  }  else {
     numbers.push(+input);
   }
 } while (input != null);
+numbers.pop();
+
+if (numbers.length != 0) {
   for (let element of numbers) {
     total += element;
   };
-  if (total != 0) {
-    alert(`Общая сумма чисел равна ${total}`);
-  } else {
-    alert('Вы не ввели ни одного числа!');
-  }
+  alert(`Общая сумма чисел равна ${total}`);
+} else {
+      alert('Вы не ввели ни одного числа!');
+    }
